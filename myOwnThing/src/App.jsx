cb618603b1c02bcbef95867b47b0ca14591b4d95
Zhi Ying Saw <his@topdanmark.sk>
2023-05-26 11:49:19 +0800

    component composition

--- myOwnThing/src/App.jsx
+++ myOwnThing/src/App.jsx
@@ -1,30 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "own thing"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "dog",
-      breed: "corgi",
-    }),
-    React.createElement(Pet, {
-      name: "Kiki",
-      animal: "cat",
-      breed: "bengal",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt me</h1>
+      <SearchParams />
+    </div>
+  )
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App />);
